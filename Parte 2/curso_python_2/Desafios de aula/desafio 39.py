de3aa815fac5d43e28dac80cb2fc4994e0bcb785
Reulyson Wanzeler <reulyson@gmail.com>
2021-05-19 11:42:51 -0400

    corrigindo a variável 'idade' e adicionando a variável 'ano'

--- Parte 2/curso_python_2/Desafios de aula/desafio 39.py
+++ Parte 2/curso_python_2/Desafios de aula/desafio 39.py
@@ -2,7 +2,8 @@
 #vai se alistar ao serviço militar, se é a hora exata de se alistar ou se já passou do tempo do alistamento. 
 #Seu programa também deverá mostrar o tempo que falta ou que passou do prazo.
 
-idade = int(input("Qual sua idade? "))
+ano = int(input("Em que ano você nasceu? "))
+idade =  2021- ano
 if idade < 18:
     tempo = 18 - idade
     print(f"Você ainda não está no tempo de se alistar. Ainda faltam {tempo} anos para isso.")
