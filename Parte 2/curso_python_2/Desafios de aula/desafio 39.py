7c73849682a79ed74ec97eef8a4cc484bdbe71d3
Reulyson Wanzeler <reulyson@gmail.com>
2021-05-19 12:22:48 -0400

    aprimorando o script

--- Parte 2/curso_python_2/Desafios de aula/desafio 39.py
+++ Parte 2/curso_python_2/Desafios de aula/desafio 39.py
@@ -2,20 +2,26 @@
 #vai se alistar ao serviço militar, se é a hora exata de se alistar ou se já passou do tempo do alistamento. 
 #Seu programa também deverá mostrar o tempo que falta ou que passou do prazo.
 
-#biblioteca para manter o ano sempre atualizado
+#importando biblioteca date para informar ano atual
 from datetime import date
+from typing import List
 
-#criando a variáveis para gerar a idade da pessoa
+#variaveis para idade
 atual = date.today().year
-ano = int(input("Em que ano você nasceu? "))
-idade =  atual - ano
+ano = int(input("Em qual ano você nasceu? "))
+idade = atual - ano
+print(f"Quem nasceu em {ano} tem {idade} em {atual}.")
 
-#veriicando a idade para o alistamento
+#verificado a idade e ano para alistamento
 if idade < 18:
     tempo = 18 - idade
     print(f"Você ainda não está no tempo de se alistar. Ainda faltam {tempo} anos para isso.")
+    alist = atual + tempo
+    print(f"Seu alistamento será em {alist}")
 elif idade == 18:
     print("Você ja está com idade para se alistar")
 else:
     tempo = idade - 18
     print(f"Você está atrasado, seu alistamento deveria ter ocorrido à {tempo} anos!")
+    alist = atual - tempo
+    print(f"Seu alistamento foi em {alist}")
