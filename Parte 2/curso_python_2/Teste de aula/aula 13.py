f62a29975948d0638a4cc26a0751a74f46d1005c
Reulyson Wanzeler <reulyson@gmail.com>
2021-05-22 09:28:18 -0400

    revisão da aula 13

--- Parte 2/curso_python_2/Teste de aula/aula 13.py
+++ Parte 2/curso_python_2/Teste de aula/aula 13.py
@@ -1,14 +1,19 @@
+#for é um laço com variável de controle
+
+#exemplo1
 '''
-for c in range(6,0): # contagem considera um numero a menos no final #para fazer a contagem de tras para frente no final se adiciona -1
+for c in range(6, 0, -1): # contagem considera um numero a menos no final #para fazer a contagem de tras para frente no final se adiciona -1
     print(c)
 print('Fim')
 '''
+#exemplo2
 '''
 n = int(input('Digite um numero: '))
 for c in range(0, n + 1): #faz uma contagem ate o numero escrito. pois a variavel n esta somando +1.
     print(c)
 print('Fim')
 '''
+#exemplo3
 '''
 i = int(input('Inicio: ')) #determina o inico da contagem
 f = int(input('Fim: ')) #o fim da contagem
@@ -17,11 +22,13 @@ for c in range(i, f + 1, p): #determinando o funcionamento
     print(c)
 print('Fim')
 '''
+#exemplo4
 '''
 for c in range(0, 3): #determinha quantos numeros serão digitados
     n = int(input('Digite um numero: '))
 print('Fim')
 '''
+#exemplo5
 s = 0
 for c in range(0, 3): #determina quantos numeros serão digitados
     n = int(input('Digite um numero: '))
